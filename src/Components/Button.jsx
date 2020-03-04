c996f20638a21235aae99f0de1e0e6896e47e4fd
Austin Zuffi <austinzuffi@gmail.com>
2020-03-03 20:14:29 -0700

    manifest didnt get added last commit? button added for download m3u

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,19 +2,27 @@ import React from 'react'
 import { FiPlay } from 'react-icons/fi'
 import styled from 'styled-components';
 import Spinner from 'react-spinkit';
+import { MdPlayForWork } from 'react-icons/md'
 
-const Button = ({playing, handleClick}) => {
-  const Icon = styled.div`
-    width: 15%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-  `;
+const Icon = styled.div`
+  width: 15%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Button = ({playing, handleClick, trackurl}) => {
+  const m3u = trackurl + ".m3u";
+
   return <Icon>
     {(!playing) 
       ? <FiPlay color= "white" size="2rem" onClick={() => handleClick()}/> 
       : <Spinner name="line-scale-pulse-out" color="white" onClick={() => handleClick()}/>}
+
+      <a href={m3u}>
+        <MdPlayForWork color= "white" size="2rem"/> 
+      </a>
     </Icon>
 };
 
