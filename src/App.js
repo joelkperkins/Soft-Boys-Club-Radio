f4b9d9a977bb5a0e389fa36caa1b3688ca273ad3
Austin Zuffi <austinzuffi@gmail.com>
2020-03-15 22:38:17 -0700

    hack to get a single tape into array so it doesnt break our map functions

--- src/App.js
+++ src/App.js
@@ -21,29 +21,49 @@ const App = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
+
+      const result = await axios.get(
         'https://icecast.softboys.club:18000/status-json.xsl',
       );
+
+      //immediately invoked function expression
       const response = {
         adminEmail: result.data.icestats.admin,
-        tracks: result.data.icestats.source.map((s, i) => {
-          return {
-            id: 'track' + i,
-            genre: s.genre || null,
-            title: s.title || '<no track data>',
-            url: s.listenurl || null,
-            date: s.stream_start || null,
-            station: s.server_name || null,
-            desc: s.server_description || null,
-            heardBy: s.listener_peak || null,
-            type: s.server_type || null
+        tracks: !Array.isArray(result.data.icestats.source)
+          ? ((s=result.data.icestats.source) => (
+            [{
+              id: 'track0',
+              genre: s.genre || null,
+              title: s.title || '<no track data>',
+              url: s.listenurl || null,
+              date: s.stream_start || null,
+              station: s.server_name || null,
+              desc: s.server_description || null,
+              heardBy: s.listener_peak || null,
+              type: s.server_type || null
+            }]
+          ))()
+          : result.data.icestats.source.map((s, i) => {
+            return {
+              id: 'track' + i,
+              genre: s.genre || null,
+              title: s.title || '<no track data>',
+              url: s.listenurl || null,
+              date: s.stream_start || null,
+              station: s.server_name || null,
+              desc: s.server_description || null,
+              heardBy: s.listener_peak || null,
+              type: s.server_type || null
+            }
           }
-        })
+        )
       }
+
       setData(response);
     };
     fetchData();
   }, []);
+
   return (
     <Body>
       <Header />
